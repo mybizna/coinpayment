7ad2f41dc5d00a9b9da7ea2c32c294aa710eab4e
Dedan <dedanirungu@gmail.com>
2024-08-12 12:24:19 +0300

    Changes to files: Classes/CoinpaymentProcessor.php Http/Controllers/CoinpaymentController.php Http/Controllers/Controllers/CoinpaymentController.php Models/.gitkeep Models/Coinpayment.php Tests/Feature/.gitkeep Tests/Unit/.gitkeep composer.json

---
+++ app/Filament/Resources/CoinpaymentResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Modules\Coinpayment\Filament\Resources;
+
+use Modules\Coinpayment\Filament\Resources\CoinpaymentResource\Pages;
+use Modules\Coinpayment\Filament\Resources\CoinpaymentResource\RelationManagers;
+use Modules\Coinpayment\Models\Coinpayment;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CoinpaymentResource extends Resource
+{
+    protected static ?string $model = Coinpayment::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCoinpayments::route('/'),
+            'create' => Pages\CreateCoinpayment::route('/create'),
+            'edit' => Pages\EditCoinpayment::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/CoinpaymentResource/Pages/CreateCoinpayment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\Coinpayment\Filament\Resources\CoinpaymentResource\Pages;
+
+use Modules\Coinpayment\Filament\Resources\CoinpaymentResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCoinpayment extends CreateRecord
+{
+    protected static string $resource = CoinpaymentResource::class;
+}
