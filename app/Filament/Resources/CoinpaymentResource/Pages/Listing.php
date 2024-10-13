1ea16b721791cba8f02cd2a002ab32d219727345
Dedan <dedanirungu@gmail.com>
2024-10-13 16:27:30 +0300

    Release 2024.13

---
+++ app/Filament/Resources/CoinpaymentResource/Pages/Listing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Coinpayment\Filament\Resources\CoinpaymentResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\ListingBase;
+
+// Class List that extends ListBase
+class Listing extends ListingBase
+{
+}
