f8cbb46e8cd660202ef495f3070bbe4a8fc60b2a
Dedan <dedanirungu@gmail.com>
2025-01-18 14:22:46 +0300

    Changes to add database migration to model

--- app/Models/Coinpayment.php
+++ app/Models/Coinpayment.php
@@ -3,6 +3,7 @@
 namespace Modules\Coinpayment\Models;
 
 use Modules\Base\Models\BaseModel;
+use Illuminate\Database\Schema\Blueprint;
 
 class Coinpayment extends BaseModel
 {
@@ -20,4 +21,26 @@ class Coinpayment extends BaseModel
      * @var string
      */
     protected $table = "coinpayment_coinpayment";
+
+    public function migration(Blueprint $table): void
+    {
+        $table->id();
+
+        $table->string('txn_id')->nullable();
+        $table->string('ipn_version')->nullable();
+        $table->string('ipn_type')->nullable();
+        $table->string('ipn_mode')->nullable();
+        $table->string('ipn_id')->nullable();
+        $table->string('merchant')->nullable();
+        $table->string('item_name')->nullable();
+        $table->string('item_number')->nullable();
+        $table->string('amount')->nullable();
+        $table->string('amounti')->nullable();
+        $table->string('currency')->nullable();
+        $table->string('status')->nullable();
+        $table->string('status_text')->nullable();
+        $table->foreignId('partner_id')->nullable()->constrained(table: 'partner_partner')->onDelete('set null');
+        $table->foreignId('payment_id')->nullable()->constrained(table: 'account_payment')->onDelete('set null');
+
+    }
 }
